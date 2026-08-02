94a651b6d8dce30430a1136033f834c01753519d
santhosh7761 <santhu.ch111@gmail.com>
2026-08-02 18:24:19 +0000

    Added Docker support and HelloController

---
+++ src/main/java/com/devops/springbootdevopsdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.devops.springbootdevopsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Docker is Working!";
+    }
+}
