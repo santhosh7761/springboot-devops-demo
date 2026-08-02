011dbf7a5f9b56bcd11f4316ecff50adf87f45d6
Ubuntu <ubuntu@ip-172-31-32-168.eu-north-1.compute.internal>
2026-08-02 12:20:22 +0000

    Initial Spring Boot application

---
+++ src/main/java/com/devops/springbootdevopsdemo/SpringbootDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devops.springbootdevopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevopsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/springbootdevopsdemo/SpringbootDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.springbootdevopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
